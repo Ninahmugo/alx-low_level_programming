338ebbaf2d93695e20acf97d10d574fb73b22774
Ninahmugo <ninahmugo1@gmail.com>
2023-03-31 03:45:22 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-
 	a[2] = 1024;
+
 	p = &n;
   /*
    * write your line of code here...
@@ -20,4 +19,5 @@ int main(void)
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
+}
 
